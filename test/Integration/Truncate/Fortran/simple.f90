662c4fac25f2dc02e62db27a93664a0c97447cf7
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2025-06-04 11:49:23 +0900

    Add -O0 test

--- test/Integration/Truncate/Fortran/simple.f90
+++ test/Integration/Truncate/Fortran/simple.f90
@@ -1,3 +1,4 @@
+! RUN: %flang -O0 %s -o %t.a.out %loadFlangRaptor %linkRaptorRT -lm -lmpfr && %t.a.out 100000 2 | FileCheck %s
 ! RUN: %flang -O1 %s -o %t.a.out %loadFlangRaptor %linkRaptorRT -lm -lmpfr && %t.a.out 100000 2 | FileCheck %s
 ! RUN: %flang -O2 %s -o %t.a.out %loadFlangRaptor %linkRaptorRT -lm -lmpfr && %t.a.out 100000 2 | FileCheck %s
 ! RUN: %flang -O3 %s -o %t.a.out %loadFlangRaptor %linkRaptorRT -lm -lmpfr && %t.a.out 100000 2 | FileCheck %s
